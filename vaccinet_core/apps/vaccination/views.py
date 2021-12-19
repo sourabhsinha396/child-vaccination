791c0beb20159236677ed96a9881343675e1c90f
nofoobar <ilovedjango3000@gmail.com>
2021-12-19 21:53:23 +0530

    add contact us page logic

--- vaccinet_core/apps/vaccination/views.py
+++ vaccinet_core/apps/vaccination/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+
+from .forms import ContactForm
 
 
 def ping(request):
@@ -8,3 +11,14 @@ def ping(request):
 
 def homepage(request):
     return render(request, 'vaccination/homepage.html')
+
+
+def contact_us(request):
+    messages.success(request, "Your message was succesfully submitted.")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            pass
+    return render(request, 'vaccination/contact_us.html')
