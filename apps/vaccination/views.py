60a80bc30fea2bca57b1657313aa35e331c36c81
nofoobar <ilovedjango3000@gmail.com>
2021-12-27 23:54:28 +0530

    add contact us backend logic and remove pycache files

--- apps/vaccination/views.py
+++ apps/vaccination/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import ContactForm
 
 def ping(request):
     return HttpResponse('pong')
@@ -8,3 +9,14 @@ def ping(request):
 
 def homepage(request):
     return render(request, 'vaccination/homepage.html')
+
+
+def contact_us(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            pass
+        return render(request, 'vaccination/contact_us.html') 
+    return render(request, 'vaccination/contact_us.html') 
