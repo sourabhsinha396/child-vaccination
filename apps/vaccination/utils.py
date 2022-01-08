2572d035577a17047ba1d46e66ead1ad591b2a4a
nofoobar <ilovedjango3000@gmail.com>
2022-01-08 11:18:41 +0530

    decrease random number count to accomodate for hypens in name

--- apps/vaccination/utils.py
+++ apps/vaccination/utils.py
@@ -1,7 +1,7 @@
 import random
 from django.utils.text import slugify
 
-def random_slug(name="user",digits=11):
+def random_slug(name="user",digits=10):
     lower = 10**(digits-1)
     upper = 10**digits - 1
     return slugify(name[:8] + str(random.randint(lower, upper)))
